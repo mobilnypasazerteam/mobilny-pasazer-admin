c824ff665d39a9b17ab33b70087593ba2151f82b
Ewa Raczynska-Bulawa <tanne@autograf.pl>
2019-06-22 11:04:57 +0200

    Init

---
+++ src/main/java/pl/sda/mobilnypasazeradmin/MobilnyPasazerAdminApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.mobilnypasazeradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilnyPasazerAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( MobilnyPasazerAdminApplication.class, args );
+    }
+
+}
